f857217fc0a30a06892fa81ff981642fac287b7d
zigzagzone <zigzagzone@gmail.com>
2022-07-17 08:50:48 +0700

    Initial commit

---
+++ src/BasicComponent/HelloClassComponent.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+// To do write you class component here
+
+export default HelloClassComponent
---
+++ src/BasicComponent/HelloFunctionComponent.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+// To do write you function component here
+
+export default HelloFunctionComponent
